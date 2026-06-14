ed5fe2cfc8a6bc52d315d304b5607d7f6699b30f
Lesha zheltov <leshazheltov@MacBook-Air-Lesha.local>
2026-06-14 14:57:47 +0900

    Первое задание

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,24 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // implement algorithm here
+        UserService userDao = new UserServiceImpl();
+
+        userDao.createUsersTable();
+        userDao.saveUser("Alex", "Long", (byte) 29);
+        userDao.saveUser("Albert", "Double", (byte) 32);
+        userDao.saveUser("Ash", "Float", (byte)17);
+        userDao.saveUser("Almond", "Byte", (byte)20);
+
+        userDao.getAllUsers();
+       userDao.cleanUsersTable();
+       userDao.dropUsersTable();
+
+
+
     }
 }
